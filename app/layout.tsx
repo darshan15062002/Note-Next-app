d4340dcfdb5b868b6c4040f908eb6e391d076b96
darshan15062002 <darshanjain15062002@gmail.com>
2023-10-16 22:49:04 +0530

    auth using clerk and backend convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,48 @@
-import { ThemeProvider } from '@/components/providers/them-provider'
-import './globals.css'
-import type { Metadata } from 'next'
+import { ThemeProvider } from "@/components/providers/them-provider";
+import "./globals.css";
+import type { Metadata } from "next";
 
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Note App',
-  description: 'Note creatations helps you better and faster work',
-  icons:{
-    icon:[
+  title: "Note App",
+  description: "Note creatations helps you better and faster work",
+  icons: {
+    icon: [
       {
-      media:"(prefers-color-scheme:light)",
-      url:'/log.png',
-      href:'/log.png'
-    },
-    {
-      media:"(prefers-color-scheme:dark)",
-      url:'/log.png',
-      href:'/log.png'
-    },
-
-  ]
-  }
-}
+        media: "(prefers-color-scheme:light)",
+        url: "/log.png",
+        href: "/log.png",
+      },
+      {
+        media: "(prefers-color-scheme:dark)",
+        url: "/log.png",
+        href: "/log.png",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider 
-        attribute='class'
-        defaultTheme='system'
-        enableSystem
-        disableTransitionOnChange
-        storageKey='note-theme'>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="note-theme"
+        >
+          {children}
         </ThemeProvider>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://funky-thrush-20.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
