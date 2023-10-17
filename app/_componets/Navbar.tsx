767a96c775580cdfba33336e23167520349d1e1a
darshan15062002 <darshanjain15062002@gmail.com>
2023-10-17 12:27:37 +0530

    auth

--- app/_componets/Navbar.tsx
+++ app/_componets/Navbar.tsx
@@ -1,25 +1,41 @@
-"use client"
-import { useScrollTop } from '@/hooks/use-scroll-top'
-import { cn } from '@/lib/utils'
-import React from 'react'
-import { Logo } from './Logo'
-import { ModeToggle } from '@/components/mode-toggle'
+"use client";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { useConvexAuth } from "convex/react";
+import { Logo } from "./Logo";
+import { ModeToggle } from "@/components/mode-toggle";
+import { SignIn, SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Ghost } from "lucide-react";
 
 function Navbar() {
-    const scrolled = useScrollTop() 
-    
-    
-    return (
-        <div className={cn("z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6",
-        scrolled && "border-b shadow-sm")}>
-            <Logo/>
-            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-                <ModeToggle/>
+  const scrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
-            </div>
-
-        </div>
-    )
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <p>Loading....</p>}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        <ModeToggle />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
